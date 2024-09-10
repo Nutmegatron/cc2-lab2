5126feeee7587a7d864527008ee3e94f03e25615
Nutmegatron <cadg.toralba@gmail.com>
2024-09-10 17:49:02 +0800

    Added files used for my Lab 2 assignment

---
+++ Carlinbhert-cc2-lab2/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        Length and Width values
+        int length = 20;
+        int width = 15;
+
+//        Displaying Length/Width of the garden
+        System.out.println("Length: " + length + " meters");
+        System.out.println("Width: " + width + " meters");
+
+//        Calculating the Area of the garden
+        System.out.println("Area: " + (length * width) + " meters squared");
+//        Calculating the Perimeter of the garden
+        System.out.println("Perimeter: " + (2 * (length + width)) + " meters");
+    }
+}
